91f7979fc3a840920f15909a04b51a279785c0a2
Pavel Uspenskij <uspenskiydj@gmail.com>
2019-07-12 20:39:41 +0400

    1. Переименование файлов: FXMLMazeController.java --> MainController.java FXMLMaze.fxml --> main.fxml 2. Распределение файлов по каталогам: maze\gui\FXMLMazeController.java --> maze\controller\MainController.java maze\gui\FXMLMaze.fxml --> maze\fxml\main.fxml maze\gui\Main.java --> maze\start\Main.fxml 3. Удаление старых файлов: maze\gui\FXMLMazeController.java maze\gui\FXMLMaze.fxml maze\gui\Main.java 4. Первоначальное редактирование файлов, удаление лишних строк

---
+++ src/maze/controller/MainController.java
@@ -0,0 +1,9 @@
+package maze.controller;
+
+import javafx.event.ActionEvent;
+
+
+public class MainController {
+    
+
+}
---
+++ src/maze/start/Main.java
@@ -0,0 +1,25 @@
+package maze.start;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/maze/fxml/main.fxml"));
+        Scene scene = new Scene(root, 1150, 720);
+        
+        stage.setTitle("Моделирование лабиринта и поиск выхода из него");
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}

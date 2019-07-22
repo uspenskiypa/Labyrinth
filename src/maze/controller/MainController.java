6bba9fd8e37f37e6e274a0f34670b4fadc67099c
Pavel Uspenskij <uspenskiydj@gmail.com>
2019-07-22 20:12:13 +0400

    Написание бработчика нажатия на кнопки "Начать проходить", "Пройти", "Сделать шаг". Добавление методов для визуализация мыши в лабиринте.

--- src/maze/controller/MainController.java
+++ src/maze/controller/MainController.java
@@ -5,10 +5,14 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import maze.objects.Labyrinth;
+import maze.objects.Mouse;
 
 
 public class MainController {
@@ -17,15 +21,28 @@ public class MainController {
     
     @FXML
     private RadioButton rbAlgRight; //переключатель "Правой руки"
-    
+     
     @FXML
     private RadioButton rbModeAuto; //переключатель "Автоматический"
     
     @FXML
-    private  GridPane pnGridMaze; //переключатель "Автоматический"
+    private  GridPane pnGridMaze; //Панель с сеткой
+    
+    @FXML
+    private  Button btAutoGo; //кнопка "Пройти"
+    
+    @FXML
+    private  Button btStep; //кнопка "Сделать шаг"
+    
+    @FXML
+    private  Button btStart; //кнопка "Начать проходить"
     
     Random rn = new Random();
     Labyrinth maze;
+    Mouse mouse;
+    Image image = new Image(getClass().getResourceAsStream("/maze/icons/mouse.png"));
+    ImageView imageView = new ImageView(image);
+    
 
     //Обработчик нажатия на кнопку "Сгенерировать"
     public void btGenerateButtonAction(ActionEvent actionEvent) {
@@ -34,10 +51,10 @@ public class MainController {
         maze = new Labyrinth(columns,rows);
         pnGridMaze.getChildren().clear();
         if (rbAlgPrim.isSelected()) {
-            maze.runPrimAlg(columns, rows); //использование алгоритма Прима
+            maze.runPrimAlg(); //использование алгоритма Прима
         }           
         else {
-            maze.runKruskalAlg(columns, rows); //использование алгоритма Прима
+            maze.runKruskalAlg(); //использование алгоритма Прима
         }
         for (int i = 0; i < columns; i++) {
             for (int j = 0; j < rows; j++) {
@@ -57,21 +74,62 @@ public class MainController {
         while (firstPane.equals(secondPane)) {
             firstPane = randomStart(columns, rows, true);
         }
+        btAutoGo.setDisable(true);
+        btStep.setDisable(true);
+        btStart.setDisable(false);
     }
     
     //Обработчик нажатия на кнопку "Начать проходить"
     public void btStartButtonAction(ActionEvent actionEvent) {
-        
+        btStart.setDisable(true);     
+        if (rbModeAuto.isSelected()) {
+            btAutoGo.setDisable(false);
+        }
+        else {
+            btStep.setDisable(false);
+        }
+        int entryX = maze.getEntryX();
+        int entryY = maze.getEntryY();
+        mouse = new Mouse(entryX, entryY);
+        changeDirection(maze.getWidth()-1, maze.getHeight()-1);
+        maze.setMouse(mouse);
+        imageView.setRotate(mouse.direction * 90);
+        ((Pane)getNodeFromGridPane(pnGridMaze, maze.getExitX(), maze.getExitY())).getChildren().clear();
+        ((Pane)getNodeFromGridPane(pnGridMaze, mouse.x, mouse.y)).getChildren().add(imageView);    
     }
     
     //Обработчик нажатия на кнопку "Пройти"
     public void btAutoGoButtonAction(ActionEvent actionEvent) {
-        
+        int ExitX = maze.getExitX();
+        int ExitY = maze.getExitY();
+        do {
+            ((Pane)getNodeFromGridPane(pnGridMaze, mouse.x, mouse.y)).getChildren().clear();
+            mouse.runRightHandAlg(maze.isFrontWall(), maze.isRightWall());
+            imageView.setRotate(mouse.direction * 90);
+            ((Pane)getNodeFromGridPane(pnGridMaze, mouse.x, mouse.y)).getChildren().add(imageView);   
+        }
+        while (!(mouse.x == ExitX && mouse.y == ExitY));
+        btAutoGo.setDisable(true);
+        btStart.setDisable(false);
     }
     
     //Обработчик нажатия на кнопку "Сделать шаг"
     public void btStepButtonAction(ActionEvent actionEvent) {
-        
+        int ExitX = maze.getExitX();
+        int ExitY = maze.getExitY();
+        ((Pane)getNodeFromGridPane(pnGridMaze, mouse.x, mouse.y)).getChildren().clear();
+        if (rbAlgRight.isSelected()) {
+            mouse.runRightHandAlg(maze.isFrontWall(), maze.isRightWall());
+        }
+        else {
+            
+        }
+        imageView.setRotate(mouse.direction * 90);
+        ((Pane)getNodeFromGridPane(pnGridMaze, mouse.x, mouse.y)).getChildren().add(imageView);
+        if (mouse.x == ExitX && mouse.y == ExitY) {
+            btStep.setDisable(true);
+            btStart.setDisable(false);
+        }
     }
     
     //Обработчик нажатия на кнопку "Выход"
@@ -106,7 +164,7 @@ public class MainController {
                 break;
             }
         }
-        Pane pane = (Pane) getRandomNode(pnGridMaze, i, j);
+        Pane pane = (Pane) getNodeFromGridPane(pnGridMaze, i, j);
         pane.getStyleClass().clear();
         if (isExit) {
             pane.getStyleClass().add("maze-exit");
@@ -127,13 +185,26 @@ public class MainController {
         return pane;
     }
     
+    //Изменяет направление мыши при появлении на старте
+    public void changeDirection(int columns, int rows) {
+        if (mouse.y == rows) {
+            mouse.direction = 0;
+        }
+        else if (mouse.x == 0) {
+            mouse.direction = 1;
+        }
+        else if (mouse.y == 0) {
+            mouse.direction = 2;
+        }
+        else if (mouse.x == columns) {
+            mouse.direction = 3;
+        }   
+    }
+    
     //Возвращает объект панели по индексу
-    private Node getRandomNode(GridPane gridPane, int col, int row) {
+    private Node getNodeFromGridPane(GridPane gridPane, int col, int row) {
         for (Node node : gridPane.getChildren())
-            if (GridPane.getColumnIndex(node) != null
-                    && GridPane.getColumnIndex(node) != null
-                    && GridPane.getRowIndex(node) == row
-                    && GridPane.getColumnIndex(node) == col)
+            if (GridPane.getRowIndex(node) == row && GridPane.getColumnIndex(node) == col)
                 return node;
         return null;
     } 

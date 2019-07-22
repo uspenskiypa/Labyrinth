173512df0f3758f3b61e305afb6c50aea9c7f5ec
Pavel Uspenskij <uspenskiydj@gmail.com>
2019-07-22 20:07:19 +0400

    Добавление методов для определение наличия стены спереди и справа. Добавление геттеров и сеттеров для переменных.

--- src/maze/objects/Labyrinth.java
+++ src/maze/objects/Labyrinth.java
@@ -6,12 +6,14 @@ import java.util.Random;
 
 public class Labyrinth {
     private Location[][] maze; //двумерный массив локаций
+    private Mouse mouse; //объект класса Mouse
     private static final int[] dx = {-1, 0, 1, 0}; //изменение координат по х
     private static final int[] dy = {0, -1, 0, 1}; //изменение координат по y
     public  enum Attribute {INSIDE, OUTSIDE, BORDER}; //статус локаций
     private ArrayList<Location> borderList = new ArrayList<Location>(); //список с локациями, статус  "BORDER"
     private ArrayList<Location> insideList = new ArrayList<Location>(); //список с локациями, статус  "INSIDE"
-    private int exitX, exitY, entryX, entryY;
+    private int exitX, exitY, entryX, entryY; //координаты входа и выхода
+    private int width, height;
     
     //Конструктор. Создание массива maze и его заполнение локациями,
     //присвоение локациям координат расположения в массиве. 
@@ -24,6 +26,8 @@ public class Labyrinth {
                 maze[i][j].y = j;
             }
         }
+        width = columns;
+        height = rows;
     }
     
     //Алгоритм Прима. Локации имеют статус (атрибут). изначально их  
@@ -32,20 +36,20 @@ public class Labyrinth {
     //случайной локации присваиваем статус INSIDE, ее соседям - BORDER,
     //если они OUTSIDE, разрушаем стену между текущей локацией и случайной
     //соседней локацией со статусом INSIDE.
-    public void runPrimAlg(int width, int height) {
+    public void runPrimAlg() {
         int count = 0; //случайное число для выбора локации
         Random rn = new Random(); //объект класса Random
         Location currentLocation; //текущая локация
         Location randomLocation; //соседняя локация
-        currentLocation = maze[rn.nextInt(width)][rn.nextInt(height)];
+        currentLocation = maze[rn.nextInt(getWidth())][rn.nextInt(getHeight())];
         currentLocation.attribute = Attribute.INSIDE;
-        changeLocationStatus(currentLocation, width, height);        
+        changeLocationStatus(currentLocation);        
         while(!borderList.isEmpty()) {
             count = rn.nextInt(borderList.size());    
             currentLocation = borderList.get(count);
             currentLocation.attribute = Attribute.INSIDE;
             borderList.remove(count);
-            changeLocationStatus(currentLocation, width, height);
+            changeLocationStatus(currentLocation);
             count = rn.nextInt(insideList.size());
             randomLocation = insideList.get(count);
             breakWall(currentLocation, randomLocation);
@@ -58,16 +62,16 @@ public class Labyrinth {
     //эта стена. Список со стенами случайно перетасовывается, после чего каждая
     //стена проверяется: если между локациями по обе стороны от этой стены 
     //нет прохода, то стена разрушается.
-    public void runKruskalAlg(int width, int height) {
+    public void runKruskalAlg() {
         LinkedList<Location[]> wallList = new LinkedList<Location[]>(); //список со всеми стенами
-        for (int i = 0; i < width; i++) { //Добавляем горизонтальные стены
-            for (int j = 0; j < height-1; j++) {
+        for (int i = 0; i < getWidth(); i++) { //Добавляем горизонтальные стены
+            for (int j = 0; j < getHeight()-1; j++) {
                 Location [] temp = {maze[i][j], maze[i][j+1]};
                 wallList.add(temp);
             }
         }
-        for (int i = 0; i < width-1; i++) { //Добавляем вертикальные стены
-            for (int j = 0; j < height; j++) {
+        for (int i = 0; i < getWidth()-1; i++) { //Добавляем вертикальные стены
+            for (int j = 0; j < getHeight(); j++) {
                 Location [] temp = {maze[i][j], maze[i+1][j]};
                 wallList.add(temp);
             }
@@ -75,7 +79,7 @@ public class Labyrinth {
         java.util.Collections.shuffle(wallList); //перетасовка локаций
         while(!wallList.isEmpty()) {
             Location [] temp = wallList.pollFirst();
-            if (!foundWay(temp[0], temp[1], width, height)) {
+            if (!foundWay(temp[0], temp[1])) {
                 breakWall(temp[0], temp[1]);   
             }
         }
@@ -104,12 +108,12 @@ public class Labyrinth {
     }
     
     //Изменяет статус соседних локаций на BORDER, заполняет списки с локациями
-    private void changeLocationStatus(Location currentLocation, int width, int height) {
+    private void changeLocationStatus(Location currentLocation) {
         int xc, yc; //координаты соседних локаций
         for (int i = 0; i < 4; i++) {
             xc = currentLocation.x + dx[i];
             yc = currentLocation.y + dy[i];
-            if ((xc >= 0) && (yc >= 0) && (xc < width) && (yc < height)) {
+            if ((xc >= 0) && (yc >= 0) && (xc < getWidth()) && (yc < getHeight())) {
                 if (maze[xc][yc].attribute == Attribute.OUTSIDE) {
                     maze[xc][yc].attribute = Attribute.BORDER;
                     borderList.add(maze[xc][yc]);
@@ -127,7 +131,7 @@ public class Labyrinth {
     //локация добавляется в список firstList, пока не будет найдена искомая 
     //локация desiredLocation. Если все возможные локации пройдены, а путь 
     //не найдет, то такого пути нет.
-    private boolean foundWay(Location firstLocation, Location desiredLocation, int width, int height) {
+    private boolean foundWay(Location firstLocation, Location desiredLocation) {
         boolean isWall, deadlock; //наличие стены и тупика
         int xc, yc; //координаты соседних локаций
         Location currentLocation; //текущая локация
@@ -140,7 +144,7 @@ public class Labyrinth {
                 for (int j = 0; j < 4; j++) { //движемся во все стороны
                     xc = currentLocation.x + dx[j];
                     yc = currentLocation.y + dy[j];
-                    if ((xc >= 0) && (yc >= 0) && (xc < width) && (yc < height)) {
+                    if ((xc >= 0) && (yc >= 0) && (xc < getWidth()) && (yc < getHeight())) {
                         if (!firstList.contains(maze[xc][yc])) {
                             isWall = checkWall(currentLocation, maze[xc][yc]);
                             if (!isWall) {
@@ -190,8 +194,70 @@ public class Labyrinth {
         return false;
     }
     
+    public boolean isFrontWall() {
+        int x = mouse.x;
+        int y = mouse.y;
+        int p = mouse.direction;
+        switch(p) {
+            case 0: {
+                if (mouse.y == 0) return true;
+                return maze[x][y-1].downWall;
+            }
+            case 1: {
+                return maze[x][y].rightWall;
+            }
+            case 2: {
+                return maze[x][y].downWall;
+            }
+            case 3: {
+                if (mouse.x == 0) return true;
+                return maze[x-1][y].rightWall;
+            }
+        }
+        return true;
+    }
+    
+    public boolean isRightWall() {
+        int x = mouse.x;
+        int y = mouse.y;
+        int p = mouse.direction;
+        switch(p) {
+            case 0: {
+                return maze[x][y].rightWall;
+            }
+            case 1: {
+                return maze[x][y].downWall;
+            }
+            case 2: {
+                if (mouse.x == 0) return true;
+                return maze[x-1][y].rightWall;
+            }
+            case 3: {
+                if (mouse.y == 0) return true;
+                return maze[x][y-1].downWall;
+            }
+        }
+        return true;
+    }
+    
     public Location getLocation(int columns, int rows) {
         return maze[columns][rows];
+    }
+    
+    public Mouse getMouse() {
+        return mouse;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+    
+    public int getHeight() {
+        return height;
+    }
+    
+    public void setMouse(Mouse mouse) {
+        this.mouse = mouse;
     }
     
     public void setExitX(int exitX) {
@@ -208,5 +274,21 @@ public class Labyrinth {
 
     public void setEntryY(int entryY) {
         this.entryY = entryY;
+    }
+    
+    public int getExitX() {
+        return exitX;
+    }
+    
+    public int getExitY() {
+        return exitY;
+    }
+    
+    public int getEntryX() {
+        return entryX;
+    }
+    
+    public int getEntryY() {
+        return entryY;
     }
 }
